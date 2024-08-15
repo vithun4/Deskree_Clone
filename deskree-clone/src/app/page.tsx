f952a080e56ee3051e0368a8ae3396d2405421f1
vithun4 <vithun4@gmail.com>
2024-08-15 18:22:03 -0400

    modification of headers and movement of files

--- deskree-clone/src/app/page.tsx
+++ deskree-clone/src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/vercel.svg"
+              src="/nav_bar_logo_white.svg"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
